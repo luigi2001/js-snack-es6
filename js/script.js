e688f22ed0a275d807090737f75734826f05e274
luigi2001 <luigitanga@virgilio.it>
2021-06-16 17:34:23 +0200

    add esercizio 2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 /* Snack 1 */
 
-const biciCorsa = [
+/*const biciCorsa = [
     {
         nome : 'graziella',
         peso : 55
@@ -24,25 +24,67 @@ for(let i = 0; i < biciCorsa.length; i++){
     if(biciCorsa[i].peso < pesoMin.peso){
         pesoMin = biciCorsa[i];
     }
-}
+}*/
 
 /*destructuring*/
 
-const {nome,peso} = pesoMin;
+/*const {nome,peso} = pesoMin;
 
-document.getElementById('destructuring').innerHTML = nome + ' ' + peso;
+document.getElementById('destructuring').innerHTML = nome + ' ' + peso;*/
 
 
 /*template literal*/
 
-document.getElementById('templateLiteral').innerHTML = 
+/*document.getElementById('templateLiteral').innerHTML = 
 
     `
     <ul>
        <li> Nome bici: ${nome} </li>
        <li> Peso bici: ${peso} </li>
     </ul>
-    `;
+`;*/
+
+
+
+
+/* Snack 2 */
+
+const squadreCalcio = [
+    {
+        nome : 'Roma',
+        punti : 0,
+        falli : 0
+    },
+
+    {
+        nome : 'Foggia',
+        punti : 0,
+        falli : 0
+    },
+
+    {
+        nome : 'Brescia',
+        punti : 0,
+        falli : 0
+    }
+];
+
+const numeriRandom = (min,max) => Math.floor(Math.random() * (max - min + 1) ) + min;
+
+const nomeFalli = [];
+
+for(let i = 0; i < squadreCalcio.length; i++){
+    squadreCalcio[i].punti = numeriRandom (0,100);
+    squadreCalcio[i].falli = numeriRandom(0,20);
+    const {nome,falli} = squadreCalcio[i];
+    nomeFalli.push({
+        nome,
+        falli
+    });
+}
+
+console.log(nomeFalli);
+
 
 
 
